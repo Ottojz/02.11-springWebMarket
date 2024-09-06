a7cb2afd1ad8ec3651cbc6e0836582af1ddca878
Вадим <ottojz@yandex.ru>
2024-09-06 12:24:01 +0300

    Initial commit

---
+++ src/main/java/pro/sky/springWebMarket/SpringWebMarketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springWebMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMarketApplication.class, args);
+	}
+
+}
